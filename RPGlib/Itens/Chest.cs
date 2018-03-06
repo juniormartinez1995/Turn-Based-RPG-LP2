9a780cfcd03c29dcc7116ced4b21b94b12f96573
LisAzevedo <lisazevedo3@gmail.com>
2018-03-05 23:47:40 -0300

    Add umas coisas

---
+++ RPGlib/Itens/Chest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGlib.Itens
+{
+    public class Chest : MapElement
+    {
+        public Chest(int var_x, int var_y)
+        {
+            this.x = var_x;
+            this.y = var_y;
+        }
+
+
+    }
+}
+
+
+
+
---
+++ RPGlib/Itens/RandomElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGlib.Itens
+{
+    public static class RandomElement
+    {
+        private static Random Aleatory = new Random();
+
+        public static int Limiter(int MinimumValue, int MaximumValue)
+        {
+            return Aleatory.Next(MinimumValue, MaximumValue);
+        }
+    }
+}
+
+
+
