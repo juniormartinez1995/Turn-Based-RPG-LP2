88dac1244dc5aa47b6673da12ba82a2b2bdf9a3c
caio mendes mendes <caiosmendes@hotmail.com>
2018-04-09 20:54:14 -0300

    Revisao controller battle e Mob

--- RPG LP2/Controller/BattleController.cs
+++ RPG LP2/Controller/BattleController.cs
@@ -18,26 +18,30 @@ namespace RPG_LP2
     public static class BattleController
     {
         // public static Page TelaAtual { get; set; }
-      
+
         public static void InicializeBattle(Character person, Mob mob)
         {
-            CheckTurn(person,mob);
-        }
-       // BattleScreen screen;
-        public static void CheckTurn(Character person, Mob mob) // checa se é o turno do mob ou do player
-        {
             int turn = 0;
-
+            CheckTurn(person, mob,turn);
+        }
+        // BattleScreen screen;
+   
+        public static void CheckTurn(Character person, Mob mob,int turn) // checa se é o turno do mob ou do player
+        {
+        
+           
             while (FinishBattle(person, mob)) {
 
                 if (turn % 2 == 0) {
-                    PlayerTurn(person, mob);
                     turn++;
+                    PlayerTurn(person, mob,turn);
+                  
                 }
 
                 else {
-                    MobTurn(person, mob);
                     turn++;
+                    MobTurn(person, mob, turn);
+                    
                 }
 
             }
@@ -45,65 +49,75 @@ namespace RPG_LP2
 
         }
 
-        public static void PlayerTurn(Character person, Mob mob)
+        public static void PlayerTurn(Character person, Mob mob,int turn)
         {
 
-            if (true) // OLHA ISSO AQUI
-            {
-                Debug.WriteLine("Entrei aqui");
+           // if (true) // OLHA ISSO AQUI
+          //  {
+                Debug.WriteLine("Life Mob");
                 mob.HP -= person.BasicSkill();
+            Debug.WriteLine(mob.HP);
+                CheckTurn(person,mob,turn);
 
-            }
+
+
+          //  }
         }
 
-        public static void MobTurn(Character person, Mob mob)
+        public static void MobTurn(Character person, Mob mob, int turn)
         {
-            person.CurrentHP -= mob.SkillBasic();
 
+            Debug.WriteLine("Life person");
+            person.CurrentHP -= mob.Skills();
+            Debug.WriteLine(person.CurrentHP);
+            CheckTurn(person, mob,turn);
         }
-
-        public static bool FinishBattle(Character person, Mob mob)
-        {
-
-            if (mob.IsDead())
-            {
-                int xpGain = 0;
-                if (mob is Boss)
-                {
-                    xpGain = 50;
-                }
-                else
-                {
-                    xpGain = 30; 
-                }
-                person.LevelUp(xpGain);
-                return false;
-                
-                // chama musicazona, chama run 
-            }
-
-            if (person.IsDead())
-            {
-                Debug.WriteLine("hey b0ss");
-                return false;
-            }
-
-            return true;
-        }
-
-        public static void WinBattle(Character person, Mob mob)
-        {
-            Debug.WriteLine("Voce venceu !!!");
             
 
-        }
+            public static bool FinishBattle(Character person, Mob mob)
+            {
 
-        public static void LoseBattle(Character person, Mob mob)
-        {
+                if (mob.IsDead())
+                {
+                Debug.WriteLine("Person ganhou!!!");
+                    int xpGain = 0;
+                    if (mob is Boss)
+                    {
+                        xpGain = 50;
+                    }
+                    else
+                    {
+                        xpGain = 30;
+                    }
+                    person.LevelUp(xpGain);
+                    return false;
 
+                    // chama musicazona, chama run 
+                }
+
+                if (person.IsDead())
+                {
+                    Debug.WriteLine("Mob ganhou!!!");
+                    return false;
+                }
+
+                return true;
+            }
+
+            public static void WinBattle(Character person, Mob mob)
+            {
+               
+
+
+            }
+
+            public static void LoseBattle(Character person, Mob mob)
+            {
+
+            }
         }
     }
-}
+
 
     
 
