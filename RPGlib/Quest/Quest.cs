880fbb567ca6c41f35980e3c2899afae8d5fb7ad
lisazevedo <lisazevedo3@gmail.com>
2018-04-13 20:44:01 -0300

    Comecei a Quest

---
+++ RPGlib/Quest/Quest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGlib.Quest
+{
+    abstract public class Quest
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string objective { get; set; }
+
+        abstract public void StartQuest();
+
+        abstract public void EndQuest();
+    }
+
+    
+}
---
+++ RPGlib/Quest/Ribbon.cs
@@ -0,0 +1,33 @@
+﻿using RPGlib.Characters;
+using RPGlib.Itens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace RPGlib.Quest
+{
+    public class Ribbon : Item
+    {
+        public Ribbon()
+        {
+            this.ItemName = "Laco Rosa";
+            this.Description = "Laco de Caio.";
+            this.ImageItem = new BitmapImage(new Uri(@"ms-appx:///Assets/Gif_Image_Item_Quest/rosinha.png"));
+
+            //---------------------------------------------
+
+            this.Health = 0;
+            this.Armor = 0;
+            this.CriticalRate = 0;
+            this.EvasionRate = 0;
+            this.Damage = 0;
+            this.Mana = 0;
+            this.Lifesteal = 0;
+        }
+
+        public override void Effect(Character person) { }
+    }
+}
