e75c5d981f3b8c1bff86210e2f521fed5c0124bc
Leonardo de Andrade <leleo.andrade@hotmail.com>
2018-04-13 22:46:48 -0300

    ATT MERGE CORRETO

--- RPG LP2/BattleScreen.xaml.cs
+++ RPG LP2/BattleScreen.xaml.cs
@@ -86,7 +86,7 @@ namespace RPG_LP2
 
             heart_icon.Source = heart_stopped;
 
-            mob.MobDead += Mob_MobDead;
+            Mob_.MobDead += Mob_MobDead;
 
             turn = BattleController.InicializeBattle(BattlePlayer, Mob_, button);
             StartTimer();
@@ -121,7 +121,7 @@ namespace RPG_LP2
 
             if (hpBarCharacter.Value >= 0) hpBarCharacter.Value = BattlePlayer.CurrentHP;
             if (mpBarCharacter.Value >= 0) mpBarCharacter.Value = BattlePlayer.CurrentMana;
-            if (hpBarMob.Value >= 0) hpBarMob.Value = mob.HP;
+            if (hpBarMob.Value >= 0) hpBarMob.Value = Mob_.HP;
             if (BattlePlayer.CurrentHP < (BattlePlayer.MaxHealth / 2)) heart_icon.Source = heart_goON;
         }
 
