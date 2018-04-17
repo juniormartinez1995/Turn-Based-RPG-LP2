395da2f08a16710801cda51424bb58f39961c049
Vinicius Serva''git config --global user.email viniaragao2005@hotmail.com <viniaragao2005@hotmail.com>
2018-04-16 23:04:04 -0300

    Novo item boys

---
+++ RPGlib/Itens/HermesBoots.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPGlib.Itens;
+using RPGlib.Characters;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace RPGlib.Itens
+{
+    public class HermesBoots : Item
+    {
+        public HermesBoots(int lifesteal) //A MUDAR ATRIBUTOS PARA SPEED
+        {
+
+            this.ImageItem = new BitmapImage(new Uri(@"ms-appx:///Assets/hermesboots.png"));
+            this.ItemName = "Hermes's Boots";
+            this.Armor = 0;
+            this.CriticalRate = 0;
+            this.Damage = 0;
+            this.EvasionRate = 0;
+            this.Health = 0;
+            this.Mana = 0;
+            this.Lifesteal = 0;
+            this.Description = "Speed + " + lifesteal + "%";
+        }
+
+        public override void Effect(Character person)
+        {
+            person.Lifesteal += this.Lifesteal;
+
+        }
+    }
+}
